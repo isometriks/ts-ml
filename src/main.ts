7a7d91fa8a31f8f80009f835613d870b56a68f64
Craig Blanchette <craig.blanchette@gmail.com>
2024-05-02 10:56:43 -0400

    Initial vite setup

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    TS-ML
+  </div>
+`
