decf53883732eb25c53558884fc018a96ded02f9
Craig Blanchette <craig.blanchette@gmail.com>
2024-09-06 12:27:32 -0400

    try multi page vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
-  base: '/ts-ml/'
-}
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/ts-ml',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        mnist: resolve(__dirname, 'mnist.html'),
+      },
+    },
+  },
+})
